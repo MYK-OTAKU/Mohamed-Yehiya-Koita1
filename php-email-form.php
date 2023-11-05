52ccbfa32b5a212b960bd60100e5762da5525de4
MYK-OTAKU <m2017koita@gmail.com>
2023-11-05 12:48:38 +0100

    essaie
    test

--- php-email-form.php
+++ php-email-form.php
@@ -3,6 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $to = "m2017koita@gmail.com"; // Remplacez par votre adresse e-mail
     $subject = "Nouveau message depuis le formulaire de contact";
     $name = $_POST["name"];
+    $numero = $_POST["numero"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
